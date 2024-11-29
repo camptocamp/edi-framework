38c524bd600c334a84e67eaa3aece258e0e8fb1d
Simone Orsi <simahawk@gmail.com>
2024-11-29 17:08:42 +0100

    edi_sale_ubl: split input/output

--- edi_sale_ubl_oca/__manifest__.py
+++ edi_sale_ubl_oca/__manifest__.py
@@ -12,26 +12,18 @@
     "author": "Camptocamp,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/edi",
     "depends": [
-        "sale_order_import_ubl",
         "edi_sale_oca",
-        "edi_ubl_oca",
-        "edi_xml_oca",
-        "edi_exchange_template_oca",
-        "edi_exchange_template_party_data",
         "edi_state_oca",
+        "edi_ubl_oca",
         # This could be made optional
         # but the delivery part would need another source of data
         "sale_stock",
     ],
     "data": [
         "data/edi_state.xml",
-        "templates/qweb_tmpl_party.xml",
-        "templates/qweb_tmpl_order_response.xml",
         "views/sale_order.xml",
     ],
     "demo": [
         "demo/edi_exchange_type.xml",
-        "demo/edi_configuration.xml",
-        "demo/exc_templ_order_response.xml",
     ],
 }
