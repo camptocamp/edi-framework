9cd488024520d643a42f202473cb38c4f373b551
Simone Orsi <simahawk@gmail.com>
2024-11-29 17:08:42 +0100

    edi_sale_ubl: split input/output

--- edi_sale_ubl_oca/tests/common.py
+++ edi_sale_ubl_oca/tests/common.py
@@ -4,6 +4,7 @@
 
 
 import os
+import random
 
 from odoo.addons.sale_order_import_ubl.tests.common import get_test_data
 
@@ -39,31 +40,36 @@ def read_test_file(filename):
 
 class OrderInboundTestMixin:
     @classmethod
-    def _setup_inbound_order(cls, backend):
-        cls.exc_type_in = cls.env.ref("edi_sale_ubl_oca.demo_edi_exc_type_order_in")
-        cls.exc_type_out = cls.env.ref(
-            "edi_sale_ubl_oca.demo_edi_exc_type_order_response_out"
-        )
+    def _setup_inbound_record(cls, backend, type_in):
+        cls.exc_type_in = type_in
         cls.exc_type_in.backend_id = backend
-        cls.exc_type_out.backend_id = backend
         cls.exc_record_in = backend.create_record(
             cls.exc_type_in.code, {"edi_exchange_state": "input_received"}
         )
+
+    @classmethod
+    def _setup_inbound_order(cls, backend, type_in):
+        cls._setup_inbound_record(backend, type_in)
         cls.ubl_data = get_test_data(cls.env)
         # Ensure all products have a barcode
         for data in cls.ubl_data.values():
             for prod in data.products:
                 prod.barcode = prod.id * 14
+        cls.client_order_ref = str(random.randint(1000, 9999))
         fname = "UBL-Order-2.1-Example.xml"
+        cls.ubl_data[fname]["client_order_ref"] = cls.client_order_ref
         cls.order_data = cls.ubl_data[fname]
-        fcontent = cls.order_data._get_content()
+        fcontent = cls.order_data._get_content().decode()
+        fcontent = fcontent.replace(
+            "<cbc:ID>34</cbc:ID>", f"<cbc:ID>{cls.client_order_ref}</cbc:ID>"
+        )
         cls.exc_record_in._set_file_content(fcontent)
         cls.err_msg_already_imported = "Sales order has already been imported before"
 
     def _find_order(self):
         return self.env["sale.order"].search(
             [
-                ("client_order_ref", "=", self.order_data.client_order_ref),
+                ("client_order_ref", "=", self.client_order_ref),
                 ("commercial_partner_id", "=", self.order_data.partner.parent_id.id),
             ]
         )
