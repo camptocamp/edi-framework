96bd8a71bdc6326b580a0b085971d021e7ee1e2f
Hadrien Huvelle <hhu@baboum.be>
2025-03-04 11:38:25 +0100

    [FIX] refactor unsupported states

--- edi_stock_oca/models/stock_picking.py
+++ edi_stock_oca/models/stock_picking.py
@@ -1,7 +1,7 @@
 # Copyright 2022 OCA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class StockPicking(models.Model):
@@ -9,13 +9,18 @@ class StockPicking(models.Model):
     _inherit = ["stock.picking", "edi.exchange.consumer.mixin"]
 
     edi_disable_auto = fields.Boolean(
-        states={
-            "draft": [("readonly", False)],
-            "waiting": [("readonly", False)],
-            "confirmed": [("readonly", False)],
-            "assigned": [("readonly", False)],
-        },
+        compute="_compute_edi_disable_auto", store=True, readonly=True
     )
+
+    @api.depends("state")
+    def _compute_edi_disable_auto(self):
+        for record in self:
+            record.edi_disable_auto = record.state in [
+                "draft",
+                "waiting",
+                "confirmed",
+                "assigned",
+            ]
 
     def action_confirm(self):
         result = super().action_confirm()
