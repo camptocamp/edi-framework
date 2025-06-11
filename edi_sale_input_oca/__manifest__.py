da6b696e3d5919b1798a775f58e84051306608e5
Simone Orsi <simahawk@gmail.com>
2025-06-11 09:35:31 +0200

    edi_sale_input: migrate to v18

--- edi_sale_input_oca/__manifest__.py
+++ edi_sale_input_oca/__manifest__.py
@@ -6,7 +6,7 @@
     "summary": """
         Process incoming sale orders with the EDI framework.
     """,
-    "version": "14.0.1.2.0",
+    "version": "18.0.1.0.0",
     "development_status": "Alpha",
     "license": "AGPL-3",
     "author": "Camptocamp,Odoo Community Association (OCA)",
