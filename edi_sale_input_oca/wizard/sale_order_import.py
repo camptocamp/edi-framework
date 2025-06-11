b38157cf92e4812866e5b44ced485a0435e5fcca
Simone Orsi <simahawk@gmail.com>
2025-06-11 09:35:31 +0200

    edi_sale_input: migrate to v18

--- edi_sale_input_oca/wizard/sale_order_import.py
+++ edi_sale_input_oca/wizard/sale_order_import.py
@@ -16,8 +16,6 @@ class SaleOrderImport(models.TransientModel):
         vals = super()._prepare_create_order_line(
             product, uom, order, import_line, price_source
         )
-        # TODO: we should probably add an ext reference field to s.o.l. in sale_order_import
-        # and get rid of this override.
         vals["edi_id"] = import_line.get("order_line_ref") or import_line.get(
             "sequence"
         )
