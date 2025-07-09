9f828870b26d924dd0d6916ed80bb36d5012108f
phucph <phucph@trobz.com>
2025-07-09 12:31:51 +0700

    [MIG] edi_purchase_oca: Migration to 18.0
    Co-author: Simone Orsi <simahawk@gmail.com>

--- edi_purchase_oca/models/purchase_order.py
+++ edi_purchase_oca/models/purchase_order.py
@@ -10,19 +10,3 @@ class PurchaseOrder(models.Model):
         "purchase.order",
         "edi.exchange.consumer.mixin",
     ]
-
-    def button_confirm(self):
-        res = super().button_confirm()
-        # DEPRECATED: rely on `on_edi_purchase_order_state_change`
-        # provided automatically by edi.consumer.mixin
-        if self:  # TODO: is this check necessary?
-            self._event("on_button_confirm_purchase_order").notify(self)
-        return res
-
-    def button_cancel(self):
-        result = super().button_cancel()
-        # DEPRECATED: rely on `on_edi_purchase_order_state_change`
-        # provided automatically by edi.consumer.mixin
-        if self:
-            self._event("on_button_cancel_purchase_order").notify(self)
-        return result
--- edi_purchase_oca/readme/DESCRIPTION.md
+++ edi_purchase_oca/readme/DESCRIPTION.md
@@ -1,14 +1,2 @@
 This module intends to create a base to be extended by local edi rules
-for purchase.
-
-In order to add a new integration, you need to create a listener:
-
-``` python
-class MyEventListener(Component):
-    _name = "purchase.order.event.listener.demo"
-    _inherit = "base.event.listener"
-    _apply_on = ["purchase.order"]
-
-    def on_button_confirm_purchase_order(self, move):
-        """Add your code here"""
-```
+for purchase. Assign the EDI Configuration on the partner form.
