31d0a6d64f9ec650202f8925c48e262824debe8e
Ricardoalso <ricardo.almeidasoares@camptocamp.com>
2026-02-20 15:24:21 +0100

    [FIX] edi_*_oca: New way for FakeModelLoader

--- edi_core_oca/tests/test_backend_process.py
+++ edi_core_oca/tests/test_backend_process.py
@@ -15,41 +15,31 @@ from .common import EDIBackendCommonTestCase
 
 
 class EDIBackendTestProcessCase(EDIBackendCommonTestCase):
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-        vals = {
-            "model": cls.partner._name,
-            "res_id": cls.partner.id,
-            "exchange_file": base64.b64encode(b"1234"),
-        }
-        cls.record = cls.backend.create_record("test_csv_input", vals)
-
-    @classmethod
-    def _setup_records(cls):  # pylint:disable=missing-return
-        super()._setup_records()
-        # Load fake models ->/
-        cls.loader = FakeModelLoader(cls.env, cls.__module__)
-        cls.loader.backup_registry()
-        from .fake_models import EdiTestExecution
-
-        cls.loader.update_registry((EdiTestExecution,))
-        cls.ExecutionAbstractModel = cls.env["edi.framework.test.execution"]
-        cls.model = cls.env["ir.model"].search(
-            [("model", "=", "edi.framework.test.execution")]
-        )
-        cls.exchange_type_in.generate_model_id = cls.model
-        cls.exchange_type_in.process_model_id = cls.model
-        cls.exchange_type_in.input_validate_model_id = cls.model
-
-    @classmethod
-    def tearDownClass(cls):
-        cls.loader.restore_registry()
-        super().tearDownClass()
-
     def setUp(self):
         super().setUp()
+        self.loader = FakeModelLoader(self.env, self.__module__)
+        self.loader.backup_registry()
+        from .fake_models import EdiTestExecution
+
+        self.loader.update_registry((EdiTestExecution,))
+        self.ExecutionAbstractModel = self.env["edi.framework.test.execution"]
+        self.model = self.env["ir.model"].search(
+            [("model", "=", "edi.framework.test.execution")]
+        )
+        self.exchange_type_in.generate_model_id = self.model
+        self.exchange_type_in.process_model_id = self.model
+        self.exchange_type_in.input_validate_model_id = self.model
+        vals = {
+            "model": self.partner._name,
+            "res_id": self.partner.id,
+            "exchange_file": base64.b64encode(b"1234"),
+        }
+        self.record = self.backend.create_record("test_csv_input", vals)
         self.ExecutionAbstractModel.reset_faked("process")
+
+    def tearDown(self):
+        self.loader.restore_registry()
+        super().tearDown()
 
     def test_process_record(self):
         self.record.write({"edi_exchange_state": "input_received"})
